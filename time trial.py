337e3545db94c63a89c9ce1330909f919b8309a8
Xio <vinicius.jovemaprendiz.fya@gmail.com>
2019-10-17 01:05:12 -0300

    Adicionando o Cortando o Papel.

--- time trial.py
+++ time trial.py
@@ -1,96 +1,83 @@
-''' 
-
-
-'''
-
 class Grafo:
-	def __init__(self, vertices, fim = None):
-		self._vertices = vertices # um dicionário com vértices e pesos.
-		self._arestas = {}	# Dicionário contendo arestas e distâncias.
-		self._inicio = None	# Definindo o inicio da pesquisa.
-		self.fim = fim	# Definindo a cidade de destino.
+	def __init__(self, vertices, inicio = 0, fim = 0):
+		self._vertices = vertices
+		self._arestas = {}
+		self._inicio = inicio
+		self._fim = fim
 
-	def addAresta(cls, a, b, distancia):
-		''' Aresta e distância entre os vértices. '''
-		cls._arestas[(a, b)] = distancia
-		cls._arestas[(b, a)] = distancia
+	# ======================= MÉTODOS PARA OS VÉRTICES =============================
+	def setInicio(cls, inicio):
+		cls._inicio = inicio
+		cls.set_peso(inicio, 0)
 
-	def getVertices(cls):
-		''' Retorna os vértices. '''
-		return cls._vertices
 
-	def predecessor(cls, predecessor = None):
-		''' O peso do predecessor. '''
-		return predecessor
+	def getFim(cls):
+		return cls._fim
 
-	def getArestas(cls):
-		''' Retorna as arestas.'''
-		return cls._arestas
 
-	def get_lista_adjacencia_vertice(cls, vertice):
-		''' Retorna a lista de adjacência de um vértice.'''
-		return cls.getVertices()[vertice]['lista_adjacencia']
+	def setFim(cls, fim):
+		cls._fim = fim
 
-	def get_peso_vertice(cls, vertice):
-		''' Retorna o peso de um determinado vértice.'''
-		return cls.getVertices()[vertice]['peso']
 
-	def set_peso_vertice(cls, vertice, novo_peso):
-		''' Alterando o peso de um vértice. '''
-		if cls.get_peso_vertice(vertice) == None:
-			cls.getVertices()[vertice]['peso'] = novo_peso
-		elif cls.get_peso_vertice(vertice) > novo_peso:
-			cls.getVertices()[vertice]['peso'] = novo_peso
+	def get_lista_adjacencia(cls, pesquisa):
+		return cls._vertices[pesquisa]['lista_adjacencia']
+
+
+	def get_peso(cls, pesquisa):
+		return cls._vertices[pesquisa]['peso']
+
+
+	def set_peso(cls, pesquisa, peso):
+		cls._vertices[pesquisa]['peso'] = peso
+
+
+# ============================ MÉTODOS PARA AS ARESTAS ====================================
+
+
+	def setAresta(cls, a, b, distancia):
+		cls._arestas[(a,b)] = distancia
+		cls._arestas[(b,a)] = distancia
+
+
+	def getDistancia(cls, a, b):
+		return cls._arestas[(a, b)]
+
+
+# ======================================= DIJKSTRA ========================================
+
+
+	def dijkstra(cls, pesquisa, menor_caminho = 0):
+
+		lista_adjacencia = cls.get_lista_adjacencia(pesquisa)
+
+		# Peso do vértice que está sendo verificado
+		predecessor = cls.get_peso(pesquisa)
+
+		if cls.getFim() in lista_adjacencia:
+			# O peso de um vértice é dado pela soma do peso do seu antecessor
+			# com a distância entre estes vértices.
+			
+			distancia = cls.getDistancia(pesquisa, cls.getFim())
+
+			peso = predecessor + distancia
+
+			if peso < menor_caminho or menor_caminho == 0:
+				menor_caminho = peso
+				cls.set_peso(cls.getFim(), peso)
+				return menor_caminho
+
 		else:
-			cls.getVertices()[vertice]['peso'] += novo_peso
 
-	def setInicio(cls, vertice):
-		''' Define o começo da pesquisa. '''
-		cls._inicio = vertice
-		cls.getVertices()[vertice]['peso'] = 0
-
-	def dijkstra(cls, v, menor_caminho = 0, path = []):
-
-		''' Procurando o menor caminho entre duas cidades. '''
-		pesquisa = v
-		path.append(pesquisa)
-
-		# Procurando a lista de adjacencia da cidade de partida
-		lista_adjacencia = cls.get_lista_adjacencia_vertice(pesquisa)
-
-		for vertice in lista_adjacencia:
-			# Se o vértice ainda não estiver no caminho
-			if vertice not in path:
-				# Pegando o peso do predecessor...
-				predecessor = cls.get_peso_vertice(pesquisa)
-				# Definindo a distancia no peso...
-				distancia = cls.getArestas()[(pesquisa, vertice)]
-
-				# O peso do vértice é dado pelo peso do seu predecessor somado com a distância entre eles.
+			for vertice in lista_adjacencia:
+				# Definindo o peso...
+				distancia = cls.getDistancia(pesquisa, vertice)
 				peso = predecessor + distancia
+				cls.set_peso(vertice, peso)
 
-				# Se ainda não houver um peso associado ao vértice ou
-				# se o peso encontrado acima for menor do que o peso já
-				# definido, o peso deste vértice será alterado.
-				if cls.get_peso_vertice(vertice) == None:
-					cls.set_peso_vertice(vertice, peso)
-
-				elif peso < cls.get_peso_vertice(vertice):
-					cls.set_peso_vertice(vertice, peso)
-
-				if vertice == cls.fim:
-					if (cls.get_peso_vertice(pesquisa) < menor_caminho) or menor_caminho == 0:
-						menor_caminho = cls.get_peso_vertice(vertice)
-						return menor_caminho
-
-				else:
-					caminho = cls.dijkstra(vertice, menor_caminho)
+				menor_caminho = cls.dijkstra(vertice, menor_caminho)
 
 		return menor_caminho
 
-	def __str__(cls):
-		''' Printa a lista de vértices e arestas. '''
-		return str(cls.getVertices()) + '\n' + str(cls.getArestas())
 
 
 # =========================================================================================
@@ -123,14 +110,18 @@ grafo = Grafo(vertices)
 
 for cabo in range(cabos):
 	a, b, distancia = map(int, input().split())
-	grafo.getVertices()[a]['lista_adjacencia'].append(b)
-	grafo.getVertices()[b]['lista_adjacencia'].append(a)
-	grafo.addAresta(a,b,distancia)
+
+	# Um vértice entra na lista de adjacência do outro.
+	grafo.get_lista_adjacencia(a).append(b)
+	grafo.get_lista_adjacencia(b).append(a)
+
+	# Adicionando uma aresta
+	grafo.setAresta(a,b,distancia)
 
 
 # destino -> corresponde ao fim do grafo.
 destino = int(input())
-grafo.fim = destino
+grafo.setFim(destino)
 
 
 # =========================================================================================
@@ -143,14 +134,13 @@ maior_caminho = 0
 for vertice in vertices.keys():
 	if vertice != destino:
 		grafo.setInicio(vertice)
-		dijkstra = grafo.dijkstra(vertice)
+		dijkstra = grafo.dijkstra(vertice, 0)
+		print(vertice,dijkstra)
 
-		if dijkstra < menor_caminho or menor_caminho == 0:
+		if menor_caminho == 0 or dijkstra < menor_caminho and dijkstra != 0:
 			menor_caminho = dijkstra
 
 		if dijkstra > maior_caminho:
 			maior_caminho = dijkstra
-
-		print(vertice, dijkstra)
 
 print(maior_caminho - menor_caminho)
