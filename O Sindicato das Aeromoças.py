2f014642b5bef92276c777c886a604a221d59e2c
Xio <vinicius.jovemaprendiz.fya@gmail.com>
2019-10-20 22:29:17 -0300

    Alterando o arquivo 'O Sindicato das Aeromoças.py'

--- O Sindicato das Aeromoças.py
+++ O Sindicato das Aeromoças.py
@@ -1,183 +1,152 @@
+class Vertice:
+	''' Criando uma classe que defina o vértice:
+	métodos e atributos. '''
+	def __init__(self, numero):
+		self._numero = numero
+		self._peso = float('inf')
+		self._lista_adjacencia = []
+		self._verificado = False
+
+	def getNumero(cls):
+		''' Retorna o número (identificação) de um vértice. '''
+		return cls._numero
+
+	def getPeso(cls):
+		''' Retorna o peso de um vértice.'''
+		return cls._peso
+
+	def changePeso(cls, novoPeso):
+		''' Altera o peso de um vértice.'''
+		cls._peso = novoPeso
+
+	def getListaAdjacencia(cls):
+		''' Retorna a lista de adjacência de um vértice.'''
+		return cls._lista_adjacencia
+
+	def addListaAdjacencia(cls, vertice):
+		''' Adiciona um vértice à lista de adjacência.'''
+		cls._lista_adjacencia.append(vertice)
+
+
 class Grafo:
-	def __init__(self, vertices, inicio = 0, fim = 0):
+	def __init__(self, vertices, inicio = None):
 		self._vertices = vertices
 		self._arestas = {}
-		self._inicio = inicio
-		self._fim = fim
+		self.inicio = inicio
 
-	# ======================= MÉTODOS PARA OS VÉRTICES =============================
-	def setInicio(cls, inicio):
-		cls._inicio = inicio
-		cls.set_peso(inicio, 0)
-
-
-	def getFim(cls):
-		return cls._fim
-
-
-	def setFim(cls, fim):
-		cls._fim = fim
-
-
+	def getVerticeByNumber(cls, numero):
+		''' Retorna uma classe vértice. '''
+		return cls._vertices[numero]
+	
 	def getVertices(cls):
+		''' Retorna a lista de vértices de um grafo.'''
 		return cls._vertices
 
+	def getArestas(cls):
+		''' Retorna todas as arestas de um grafo.'''
+		return cls._arestas
 
-	def get_lista_adjacencia(cls, pesquisa):
-		return cls._vertices[pesquisa]['lista_adjacencia']
+	def addAresta(cls, a, b, distancia):
+		''' Adiciona uma aresta ao grafo. '''
+		if cls.getAresta(a, b) == None:
+			# Definindo a aresta para a lista de arestas de cada vértice.
+
+			# Verificando se os vértices já está incluso no 
+			# dicionário de arestas.
+			if a not in cls.getArestas().keys():
+				cls.getArestas()[a] = {}
+				cls.getArestas()[a][b] = distancia
+			elif b not in cls.getArestas()[a].keys():
+				cls.getArestas()[a][b] = distancia
+
+			if b not in cls.getArestas().keys():
+				cls.getArestas()[b] = {}
+				cls.getArestas()[b][a] = distancia
+
+			elif a not in cls.getArestas()[b].keys():
+				cls.getArestas()[b][a] = distancia
+
+			# Adicionando a na lista de adjacência de b
+			a.addListaAdjacencia(b)
+			# e b na lista de adjacẽncia de a.
+			b.addListaAdjacencia(a)
+		else:
+			# Será verificada qual aresta é menor,
+			# pois não faz sentido uma aeromoça tomar
+			# o maior caminho entre duas cidades.
+			aresta = cls.getAresta(a, b)
+
+			if distancia < aresta:
+				cls.getArestas()[a][b] = distancia
+				cls.getArestas()[b][a] = distancia
+
+	def getAresta(cls, a, b):
+		''' Retorna o comprimento de uma aresta. '''
+		try:
+			return cls._arestas[a][b]
+		except:
+			return None
 
 
-	def get_peso(cls, pesquisa):
-		return cls._vertices[pesquisa]['peso']
+def dijkstra(grafo, inicio):
+	# Definindo os pesos:
+	for p in list(grafo.getVertices().values()):
+		p.changePeso(float("inf"))
 
+	# Definindo o peso inicial como 0.
+	start = grafo.getVerticeByNumber(inicio)
+	start.changePeso(0)
 
-	def set_peso(cls, pesquisa, peso):
-		cls._vertices[pesquisa]['peso'] = peso
+	vertices = list(grafo.getVertices().values())
+	pesos = [0] * len(vertices)
+	maior_peso = 0
 
-
-	def zera_pesos(cls):
-		vertices = cls.getVertices()
-		for v in range(len(vertices)):
-			cls.set_peso(v, None)
-
-
-# ============================ MÉTODOS PARA AS ARESTAS ====================================
-
-
-	def setAresta(cls, a, b, distancia):
-		cls._arestas[(a,b)] = distancia
-		cls._arestas[(b,a)] = distancia
-
-
-	def getDistancia(cls, a, b):
-		return cls._arestas[(a, b)]
-
-
-	def setDistancia(cls, a, b, distancia):
-		cls._arestas[(a,b)] = distancia
-		cls._arestas[(b,a)] = distancia
-
-
-# ======================================= DIJKSTRA ========================================
-
-
-	def dijkstra(cls, pesquisa, menor_caminho = 0, pilha = []):
-
-		lista_adjacencia = cls.get_lista_adjacencia(pesquisa)
-
-		# Peso do vértice que está sendo verificado
-		predecessor = cls.get_peso(pesquisa)
-
-		# Adicionando o vértice aos verificados...
-		pilha.append(pesquisa)
+	while vertices:
+		lista_adjacencia = start.getListaAdjacencia()
+		vertices.remove(start)
 
 		for vertice in lista_adjacencia:
-			if vertice not in pilha:
-				# O peso de um vértice é o peso do vértice anterior
-				# somado à distância entre estes dois vértices.			
-				distancia = cls.getDistancia(pesquisa, vertice)
-				peso = predecessor + distancia
+			peso_atual = vertice.getPeso()
+			peso_temp = start.getPeso() + grafo.getAresta(start, vertice)
 
-				# Se o peso encontrado for menor do que o peso já
-				# estabelecido ou se este nó ainda não tiver um peso
-				# atribuído, o novo peso será atribuído.
-				if cls.get_peso(vertice) == None or peso < cls.get_peso(vertice):
-					cls.set_peso(vertice, peso)
+			if peso_atual == float("inf") or peso_temp < peso_atual:
+				vertice.changePeso(peso_temp)
+				pesos[vertice.getNumero()] = peso_temp
 
-				# Se o vértice final foi encontrado, vamos comparar 
-				# os caminhos a fim de encontrar o menor possível.
-				if vertice == cls.getFim():
-					if peso < menor_caminho or menor_caminho == 0:
-						menor_caminho = peso
+		if vertices:
+			start = vertices[0]
 
-				else:
-					menor_caminho = cls.dijkstra(vertice, menor_caminho)
+	for h in pesos:
+		if h > maior_peso:
+			maior_peso = h
+			
+	return maior_peso
 
 
-			# Se a lista de adjacência de um vértice foi completamente varrida,
-			# ele será removido da pilha e a busca será continuada
-			if vertice == lista_adjacencia[-1]:
-				pilha.pop()
-
-		return menor_caminho
+# ========================================================
 
 
+# Informando o número de cidades e linhas aéreas.
+cidades, linhas_aereas = [int(x) for x in input().split()]
 
-# =========================================================================================
-
-
-# Adicionando os vértices ao grafo...
-
-vertices = {} # dicionário que contém os vértices e seus pesos e listas de adjacencia
-
-cidades, cabos = map(int, input().split())
+vertices = {}
 
 for cidade in range(cidades):
-	vertices[cidade] = {} # Cria um dicionário para o vértice
-	vertices[cidade]['peso'] = None	# Estabelece o peso inicial do vértice como sendo None
-	vertices[cidade]['lista_adjacencia'] = [] # Estabelece uma lista de adjacência para o vértice
+	vertice = Vertice(cidade)
+	vertices[cidade] = vertice
 
-
-# =========================================================================================
-
-
-# Criando a instância da classe...
-
+# Criando um grafo a partir da lista de vértices
 grafo = Grafo(vertices)
 
+for i in range(linhas_aereas):
+	cidade_a, cidade_b, distancia = [int(x) for x in input().split()]
+	grafo.addAresta(grafo.getVerticeByNumber(cidade_a), grafo.getVerticeByNumber(cidade_b), distancia)
 
-# =========================================================================================
+# Procurando o caminho máximo para uma aeromoça...
+maximo = float("inf")
+for j in range(cidades):
+	a = dijkstra(grafo, j)
+	if a < maximo:
+		maximo = a
 
-
-# Adicionando as arestas ao grafo...
-
-for cabo in range(cabos):
-	a, b, distancia = map(int, input().split())
-
-	lista_adjacencia = grafo.get_lista_adjacencia(a)
-
-	if b not in lista_adjacencia:
-		# Um vértice entra na lista de adjacência do outro.
-		grafo.get_lista_adjacencia(a).append(b)
-		grafo.get_lista_adjacencia(b).append(a)
-
-		# Adicionando uma aresta
-		grafo.setAresta(a,b,distancia)
-
-	elif grafo.getDistancia(a,b) > distancia:
-		grafo.setDistancia(a, b, distancia)
-
-
-# =========================================================================================
-
-
-# Fazendo a varredura em cada vértice, com exceção do destino...
-caminho_maximo = 0
-
-
-# O final do grafo é alterado a cada iteração
-for destino in vertices.keys():
-	# destino -> corresponde ao fim do grafo.
-	grafo.setFim(destino)
-
-	# Maior distância entre as aeromoças...
-	maior_distancia = 0
-
-	for vertice in vertices.keys():
-		if vertice != destino:
-			grafo.setInicio(vertice)
-			dijkstra = grafo.dijkstra(vertice, 0)
-
-			if dijkstra > maior_distancia:
-				maior_distancia = dijkstra
-
-			# Zera
-			grafo.zera_pesos()
-
-	# O caminho máximo é a maior distância mínima
-	# que uma aeromoça pode percorrer
-
-	if caminho_maximo == 0 or caminho_maximo > maior_distancia:
-		caminho_maximo = maior_distancia
-
-print(caminho_maximo)
+print(maximo)
